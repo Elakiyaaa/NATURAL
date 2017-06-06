4d62f7b19fa1247497f656ae0e81e66bf36ca135
Elakiyaaa <elakthamizh@gmail.com>
2017-06-06 14:09:37 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,15 @@
+package natural;
+
+public class natural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int temp=0;
+for(int i=0;i<=10;i++)
+		{
+	temp=temp+i;
+		}
+System.out.println("SUM OF NATURAL NUMBER IS "+temp);
+	}
+
+}
